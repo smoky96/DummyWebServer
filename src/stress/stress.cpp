132fc9431dde9573e990e3aee6f89aaa9e7ab0ad
dong <guodong@outlook.sg>
2020-07-23 15:03:18 +0800

    refactor: rewrite stress

--- src/stress/stress.cpp
+++ src/stress/stress.cpp
@@ -1,14 +1,48 @@
+#include <netdb.h>
+
 #include "conmmon.h"
 
+#define BUFSIZE 2048
+
 /* 每个客户连接不停地向服务器发送这个请求 */
-static const char* request =
-    "GET http://192.168.226.131/ "
-    "HTTP/1.1\r\nConnection:keep-alive\r\n\r\n";
+static char request[BUFSIZE];
+static char host[BUFSIZE];
+volatile bool stop = false;
+int speed = 0;
+int bytes = 0;
+int failed = 0;
+int conn_num = 1;
+
+void alarm_handler(int signal) { stop = true; }
+
+void build_request(const char* url) {
+  memset(request, '\0', sizeof(request));
+  strcpy(request, "GET ");
+  if (strstr(url, "://") == NULL) {
+    printf("Invalid URL: %s\n", url);
+    exit(-1);
+  }
+  const char* h = strstr(url, "://") + 3;
+  if (strstr(h, "/") == NULL) {
+    printf("hostname should be end with '/'\n");
+    exit(-1);
+  }
+
+  strncpy(host, h, strcspn(h, "/"));
+  strcat(request, h + strcspn(h, "/"));
+  strcat(request, " HTTP/1.1");
+  strcat(request, "\r\n");
+  strcat(request, "User-Agent: DummyWebServer\r\n");
+  strcat(request, "Host: ");
+  strcat(request, host);
+  strcat(request, "\r\n");
+  strcat(request, "Connection: keep-alive\r\n\r\n");
+}
 
 /* 向服务器写入 len 字节的数据 */
 bool write_nbytes(int sockfd, const char* buffer, int len) {
   int bytes_write = 0;
-  printf("write out %d bytes to socket %d\n", len, sockfd);
+  // printf("write out %d bytes to socket %d\n", len, sockfd);
   while (1) {
     bytes_write = Send(sockfd, buffer, len, 0);
     if (bytes_write < 0 || bytes_write == 0) return false;
@@ -24,25 +58,33 @@ bool write_nbytes(int sockfd, const char* buffer, int len) {
 bool read_once(int sockfd, char* buffer, int len) {
   int bytes_read = 0;
   memset(buffer, '\0', len);
-  while ((bytes_read = Recv(sockfd, buffer, len, 0)) > 0)
-    printf("read in %d bytes from socket %d\n", bytes_read, sockfd);
+  while ((bytes_read = Recv(sockfd, buffer, len, 0)) > 0) {
+    // printf("read in %d bytes from socket %d\n", bytes_read, sockfd);
+    if (stop) return true;
+    bytes += bytes_read;
+  }
   if (bytes_read == -1 && errno != EAGAIN) return false;
   return true;
 }
 
 /* 向服务器发起 num 个 TCP 连接，可以通过 num 来调整测试压力 */
-void start_conn(int epollfd, int* num, const char* ip, int port) {
+void start_conn(int epollfd, int num, const char* host, int port) {
   struct sockaddr_in addr;
   bzero(&addr, sizeof(addr));
-  inet_pton(AF_INET, ip, &addr.sin_addr);
+
   addr.sin_port = htons(port);
   addr.sin_family = AF_INET;
 
-  int sockfd[*num];
-  for (int i = 0; i < *num; ++i) {
+  struct hostent* he = gethostbyname(host);
+  memcpy(&(addr.sin_addr), he->h_addr_list[0], sizeof(in_addr));
+
+  int sockfd[num];
+  for (int i = 0; i < num; ++i) {
     sockfd[i] = Socket(AF_INET, SOCK_STREAM, 0);
-    Connect(sockfd[i], &addr, sizeof(addr));
-    printf("connected %d of %d\n", i + 1, *num);
+    if (Connect(sockfd[i], &addr, sizeof(addr)) < 0) {
+      ++failed;
+    }
+    printf("connected %d of %d\n", i + 1, num);
     epoll_event event;
     event.data.fd = sockfd[i];
     event.events = EPOLLOUT | EPOLLET | EPOLLERR;
@@ -52,28 +94,35 @@ void start_conn(int epollfd, int* num, const char* ip, int port) {
 }
 
 int main(int argc, char** argv) {
-  if (argc < 4) {
-    printf("Usage: %s ip_address port_number connection_number\n",
+  if (argc < 5) {
+    printf("Usage: %s host port connection_number time(sec)\n",
            basename(argv[0]));
     return 1;
   }
 
   int epollfd = Epoll_create(5);
-  epoll_event events[10000];
-  char buffer[2048];
+  epoll_event events[100000];
+  char buffer[BUFSIZE];
 
   int num = atoi(argv[3]);
-  start_conn(epollfd, &num, argv[1], atoi(argv[2]));
+  build_request(argv[1]);
+  start_conn(epollfd, num, host, atoi(argv[2]));
 
-  while (num) {
-    int n = Epoll_wait(epollfd, events, 10000, 2000);
+  struct sigaction sa;
+  sa.sa_handler = alarm_handler;
+  sa.sa_flags = 0;
+  sigaction(SIGALRM, &sa, NULL);
+  alarm(atoi(argv[4]));
+
+  while (!stop) {
+    int n = Epoll_wait(epollfd, events, 100000, 2000);
     for (int i = 0; i < n; ++i) {
       int sockfd = events[i].data.fd;
       if (events[i].events & EPOLLIN) {
-        if (!read_once(sockfd, buffer, 2048)) {
-          RemoveFd(epollfd, sockfd);
-          --num;
-          printf("have %d socket left\n", num);
+        if (!read_once(sockfd, buffer, BUFSIZE)) {
+          // RemoveFd(epollfd, sockfd);
+          ++failed;
+          // printf("have %d socket left\n", num);
           continue;
         }
 
@@ -83,21 +132,29 @@ int main(int argc, char** argv) {
         Epoll_ctl(epollfd, EPOLL_CTL_MOD, sockfd, &event);
       } else if (events[i].events & EPOLLOUT) {
         if (!write_nbytes(sockfd, request, strlen(request))) {
-          RemoveFd(epollfd, sockfd);
-          --num;
-          printf("have %d socket left\n", num);
+          // RemoveFd(epollfd, sockfd);
+          ++failed;
+          // printf("have %d socket left\n", num);
           continue;
         }
 
+        ++speed;
         epoll_event event;
         event.data.fd = sockfd;
         event.events = EPOLLIN | EPOLLET | EPOLLERR;
         Epoll_ctl(epollfd, EPOLL_CTL_MOD, sockfd, &event);
       } else {
-        RemoveFd(epollfd, sockfd);
+        // RemoveFd(epollfd, sockfd);
+        ++failed;
       }
     }
   }
+
+  int duration = atoi(argv[4]);
+  printf(
+      "\nSpeed=%d pages/min, %.2f KB/sec. Requests: %d succeed, %d failed.\n",
+      (int)((speed + failed) / (duration / 60.0f)),
+      (float)(bytes / (float)duration / 1024.0f), speed, failed);
 
   return 0;
 }
